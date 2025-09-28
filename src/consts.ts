66b2d9e9490ef5c6fabc5c239c5d6743ab09c6ab
Shereef Marzouk <shereef.marzouk@daysmart.com>
2025-09-27 23:58:41 -0300

    Update site details and content for Teta w Gedo

--- src/consts.ts
+++ src/consts.ts
@@ -1,5 +1,6 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = "Astro Blog";
-export const SITE_DESCRIPTION = "Welcome to my website!";
+export const SITE_TITLE = "Teta w Gedo";
+export const SITE_DESCRIPTION =
+  "Welcome to Teta w Gedo! The best authentic Egyptian food in town.";
